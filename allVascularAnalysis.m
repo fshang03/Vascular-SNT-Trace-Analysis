6e7ab1fce24829263b4dd689accf4c54f6f4dba7
fshang03 <fshang3@gmail.com>
2025-08-04 13:01:10 -0400

    Add files via upload

---
+++ allVascularAnalysis.m
@@ -0,0 +1,62 @@
+clear;
+clc;
+
+[fnames, pname] = uigetfile({'*.csv';'*.xlsx'},'MultiSelect','on');
+
+for f = 1:length(fnames)
+    fromSNT=readcell(string(fnames(f)));
+
+%Get rescale z values
+ii=1;
+index=zeros(1,2);
+for i=2:size(fromSNT,1)
+    if isequal(fromSNT(i,5),"CellRescalePoint")
+        index(ii)=i;
+        ii=ii+1;
+    end
+end
+
+z1=cell2mat(fromSNT(index(1),3));
+z2=cell2mat(fromSNT(index(2),3));
+hold=z1;
+
+if z1<z2
+else
+    z1=z2; %make sure lower z value is p1
+    z2=hold;
+end
+
+hold=z2-z1; %Hold now equals max - min for normalization
+
+%Take out rescale points from table
+removepoints=fromSNT;
+removepoints(index(1),:)=[];
+removepoints(index(2)-1,:)=[];
+
+%Count number of paths
+    pathNum=0;
+for i=3:size(removepoints,1)
+    if isequal(removepoints(i,4),removepoints(i-1,4))
+    else
+        pathNum=pathNum+1;
+    end
+end
+
+% plotTheVasculature2(removepoints,pathNum,fnames); %Color coded 3D plot
+
+dataWithXYZDistance=distanceBetweenPoints2(removepoints,z1,hold); %Adds XD YD ZD and Distance between two points columns to original data
+dataWithPathDistance=distanceBetweenEndPoints(dataWithXYZDistance,pathNum);
+
+writecell(dataWithXYZDistance,"C:\Users\fshang\OneDrive - University of Rochester\Documents\Hyperglycemia Paper\"+extractBefore(string(fnames(f)),"SNT")+"Analysis.xlsx",'Sheet','Xd Yd Zd Distance');
+
+zTable=sumDistanceByZ2(dataWithXYZDistance);
+writetable(zTable,"C:\Users\fshang\OneDrive - University of Rochester\Documents\Hyperglycemia Paper\"+extractBefore(string(fnames(f)),"SNT")+"Analysis.xlsx",'Sheet','Distance by Z');
+
+[lengths, distanceByCategory]=distanceByPathLabel2(dataWithXYZDistance,pathNum);
+writecell(lengths,"C:\Users\fshang\OneDrive - University of Rochester\Documents\Hyperglycemia Paper\"+extractBefore(string(fnames(f)),"SNT")+"Analysis.xlsx",'Sheet','Path Lengths');
+writetable(distanceByCategory,"C:\Users\fshang\OneDrive - University of Rochester\Documents\Hyperglycemia Paper\"+extractBefore(string(fnames(f)),"SNT")+"Analysis.xlsx",'Sheet','Distance by Category');
+
+[theStraightPath, tortuosityByCategory]=lengthOfPath(dataWithPathDistance,lengths,pathNum);
+writecell(theStraightPath,"C:\Users\fshang\OneDrive - University of Rochester\Documents\Hyperglycemia Paper\"+extractBefore(string(fnames(f)),"SNT")+"Analysis.xlsx",'Sheet','Tortuosity');
+writecell(tortuosityByCategory,"C:\Users\fshang\OneDrive - University of Rochester\Documents\Hyperglycemia Paper\"+extractBefore(string(fnames(f)),"SNT")+"Analysis.xlsx",'Sheet','Tortuosity by Category');
+end
